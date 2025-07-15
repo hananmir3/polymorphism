2f7d2d60a8acbea96449df2fc870b4dfa8fe671d
hananmir <hananyousuf3@gmail.com>
2025-07-15 16:50:54 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            myAnimal.Speak();  // Output: The animal makes a sound.
+            myDog.Speak();     // Output: The dog barks.
+            myCat.Speak();     // Output: The cat meows.
+        }
+    }
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal makes a sound.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The dog barks.");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The cat meows.");
+        }
+    }
+
+
+
+
+
+
+
+
+}
